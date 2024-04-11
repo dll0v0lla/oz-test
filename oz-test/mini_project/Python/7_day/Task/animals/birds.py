f5172754bb2b5229810f111734777795a88c3eae
dll0v0lla <dll0v0lla@gmail.com>
2024-04-11 19:20:52 +0900

    new

---
+++ oz-test/mini_project/Python/7_day/Task/animals/birds.py
@@ -0,0 +1,6 @@
+class Eagle:
+    def __init__(self, name):
+        self.name = name
+ 
+    def speak(self):
+        return "삐약삐약"
--- oz-test/mini_project/Python/7_day/Task/mymath.py
+++ oz-test/mini_project/Python/7_day/Task/mymath.py
@@ -1,3 +1,5 @@
+import math
+
 def triangle_area(base, height):
     return 0.5 * base * height
 
